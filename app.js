6c60db5803c0f88c90ed2410ae83a20ec5f52841
Kareem Yomi <kareemyomi91@gmail.com>
2021-09-19 20:59:37 +0100

    initialize hospital project

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+require('dotenv').config();
+const path = require("path");
+const http = require("http");
+const bodyParser = require("body-parser");
+const morgan = require('morgan');
+
+const app = express();
+const server = http.createServer(app);
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+app.use(morgan('dev'))
+// set up public folder
+app.use(express.static(path.join(__dirname, "public")));
+// Static Files
+// dashboard 
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/images', express.static(__dirname + 'public/images'));
+
+const PORT = process.env.PORT || 7000;
+server.listen(PORT, console.log(`Server started on ${PORT}`));
+
